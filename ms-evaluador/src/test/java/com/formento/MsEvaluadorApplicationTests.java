e3410254ebe7d8e770b2658787828007e7376952
formento722 <formento722@gmail.com>
2023-06-04 18:58:20 -0300

    subo primera version del ms-evaluador

---
+++ ms-evaluador/src/test/java/com/formento/MsEvaluadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEvaluadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
